189ef4b4c4d880c3993bf287aa53968941160451
Graeme Fraser <39315289+gfraser88@users.noreply.github.com>
2019-11-21 19:49:23 -0500

    Updates

---
+++ precache-manifest.4221879e024dfcd2e054789063746aa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2f08e6bc9bd46ea82d2754ad755d9b",
+    "url": "/nba-app/index.html"
+  },
+  {
+    "revision": "f4f88ef0b00d84749ed7",
+    "url": "/nba-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "321fbf31908e5df485d5",
+    "url": "/nba-app/static/js/2.d4f5ee27.chunk.js"
+  },
+  {
+    "revision": "f4f88ef0b00d84749ed7",
+    "url": "/nba-app/static/js/main.dccb28fc.chunk.js"
+  },
+  {
+    "revision": "f22df3f32c84096449ee",
+    "url": "/nba-app/static/js/runtime-main.556ed540.js"
+  }
+]);
